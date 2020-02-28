76674c6c2f97c40b3fbbe535637922dd273958eb
Carrot-77 <61344086+Carrot-77@users.noreply.github.com>
2020-02-28 17:15:57 +0800

    Create 101平衡二叉树.cpp

---
+++ 101平衡二叉树.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int process(TreeNode* root) {
+        if (root == NULL) return 0;
+        int left = process(root->left);
+        if (left == -1) return -1;
+        int right = process(root->right);
+        if (right == -1) return -1;
+        return abs(left - right) > 1 ? -1 : 1 + max(left, right);
+    }
+    
+    bool isBalanced(TreeNode* root) {
+        return process(root) != -1;
+    }
+    
+};
