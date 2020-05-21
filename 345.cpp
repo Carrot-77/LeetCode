493a38385ad40ada568310dc7bd8c0508c530478
Carrot-77 <1012982871@qq.com>
2020-05-21 19:17:32 +0800

    2020-05-21

---
+++ 345.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool is_valid(char ch) {
+        if (ch < 97) ch += 32;
+        switch(ch) {
+            case 'a' :
+            case 'e' :
+            case 'i' :
+            case 'o' :
+            case 'u' : return true;
+        }
+        return false;
+    }
+    string reverseVowels(string s) {
+        for (int i = 0, j = s.size() - 1; i < j; i++, j--) {
+            while (i < j && !is_valid(s[i])) ++i;
+            while (i < j && !is_valid(s[j])) --j;
+            swap(s[i], s[j]);
+        }
+        return s;
+    }
+};
