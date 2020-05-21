2cdfe274910c448bb7b306e27d8da0b7826b43bc
Carrot-77 <1012982871@qq.com>
2020-05-21 19:37:26 +0800

    2020-05-21

---
+++ 387.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int code[256] = {0};
+        for (int i = 0; s[i]; i++) code[s[i]] += 1;
+        for (int i = 0; s[i]; i++) if (code[s[i]] == 1) return i;
+        return -1;
+    }
+};
